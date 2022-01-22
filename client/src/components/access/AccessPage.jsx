f351697c12668f2cbec9494fa6bb341faf51a830
minggu99 <tuanphamtran99@gmail.com>
2022-01-22 13:41:40 +0700

    fix height access page with auto screen

--- client/src/components/access/AccessPage.jsx
+++ client/src/components/access/AccessPage.jsx
@@ -5,31 +5,31 @@ import "./Access.css";
 
 const AccessPage = ({ children, googleId }) => {
   const { user } = useSelector((state) => state.user);
-  return (
-    <Fragment>
-      {user && !googleId && children}
-      {user && googleId && (
-        <div className="container-fluid access-denided-page my-0 py-0">
-          <div className="row">
-            <div className="col-lg-12 text-center">
+
+  const templateAlert = (title, message) => {
+    return (
+      <div className="container-fluid access-denided-page my-0 py-0">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <div className="alert-access-denied">
               <img src="/images/access_denied.png" alt="access denided" />
-              <h1>ACCESS DENIDED</h1>
+              <h1>{title}</h1>
               <p>Account not support this feature.</p>
             </div>
           </div>
         </div>
-      )}
-      {!user && (
-        <div className="container-fluid access-denided-page my-0 py-0">
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <img src="/images/access_denied.png" alt="access denided" />
-              <h1>ACCESS DENIDED</h1>
-              <p>Please login to access pages.</p>
-            </div>
-          </div>
-        </div>
-      )}
+      </div>
+    );
+  };
+
+  return (
+    <Fragment>
+      {user && !googleId && children}
+      {user &&
+        googleId &&
+        templateAlert("ACCESS DENIDED", "Account not support this feature.")}
+      {!user &&
+        templateAlert("ACCESS DENIDED", "Please login to access pages.")}
     </Fragment>
   );
 };
